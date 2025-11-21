5b0623fa6ff9def5642f71c8674f2382152ea64a
sadmann7 <sadmann898@gmail.com>
2025-11-21 11:10:43 +0600

    chore: lint

--- src/types/data-grid.ts
+++ src/types/data-grid.ts
@@ -76,24 +76,24 @@ declare module "@tanstack/react-table" {
     onCellClick?: (
       rowIndex: number,
       columnId: string,
-      event?: React.MouseEvent
+      event?: React.MouseEvent,
     ) => void;
     onCellDoubleClick?: (rowIndex: number, columnId: string) => void;
     onCellMouseDown?: (
       rowIndex: number,
       columnId: string,
-      event: React.MouseEvent
+      event: React.MouseEvent,
     ) => void;
     onCellMouseEnter?: (
       rowIndex: number,
       columnId: string,
-      event: React.MouseEvent
+      event: React.MouseEvent,
     ) => void;
     onCellMouseUp?: () => void;
     onCellContextMenu?: (
       rowIndex: number,
       columnId: string,
-      event: React.MouseEvent
+      event: React.MouseEvent,
     ) => void;
     onCellEditingStart?: (rowIndex: number, columnId: string) => void;
     onCellEditingStop?: (opts?: {
@@ -111,7 +111,7 @@ declare module "@tanstack/react-table" {
     onRowSelect?: (
       rowIndex: number,
       checked: boolean,
-      shiftKey: boolean
+      shiftKey: boolean,
     ) => void;
   }
 }
