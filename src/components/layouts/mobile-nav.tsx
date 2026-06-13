1dcec3874a66e463bc744a5c26f4f46109b68868
sadmann7 <sadmann898@gmail.com>
2026-06-13 18:54:09 +0600

    feat: update links

--- src/components/layouts/mobile-nav.tsx
+++ src/components/layouts/mobile-nav.tsx
@@ -7,19 +7,19 @@ import { siteConfig } from "@/config/site";
 
 export function MobileNav() {
   return (
-    <nav className="flex flex-col p-2">
+    <nav className="flex flex-col gap-1 px-1.5">
       {siteConfig.navLinks.map((navLink) => (
         <SheetClose key={navLink.href} asChild>
           <ActiveLink
             href={navLink.href}
-            className="w-full justify-start rounded-md font-normal"
+            className="h-auto w-full justify-start px-0 py-2 font-normal text-base hover:bg-transparent dark:hover:bg-transparent"
           >
             {navLink.label}
           </ActiveLink>
         </SheetClose>
       ))}
       <SheetClose asChild>
-        <DocsLink className="w-full justify-start rounded-md font-normal" />
+        <DocsLink className="h-auto w-full justify-start px-0 py-2 font-normal text-base hover:bg-transparent dark:hover:bg-transparent" />
       </SheetClose>
     </nav>
   );
