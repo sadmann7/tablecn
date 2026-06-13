ff7068440af941d80e20260d6736493b9c681e69
sadmann7 <sadmann898@gmail.com>
2026-06-13 20:28:58 +0600

    refactor: cleanup sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,15 +3,12 @@ import type { MetadataRoute } from "next";
 import { siteConfig } from "@/config/site";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = [
-    "",
-    "/data-grid",
-    "/data-grid-live",
-    "/data-grid-multiplayer",
-  ].map((route) => ({
-    url: `${siteConfig.url}${route}`,
-    lastModified: new Date().toISOString(),
-  }));
+  const routes = ["", ...siteConfig.navLinks.map(({ href }) => href)].map(
+    (route) => ({
+      url: `${siteConfig.url}${route}`,
+      lastModified: new Date().toISOString(),
+    }),
+  );
 
   return [...routes];
 }
