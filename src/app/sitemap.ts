5e58114bda0c781a0a802f62a5fec88af727bd2d
sadmann7 <sadmann898@gmail.com>
2026-06-13 20:16:18 +0600

    chore(layout): update sitemap and keywords

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,12 @@ import type { MetadataRoute } from "next";
 import { siteConfig } from "@/config/site";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = [""].map((route) => ({
+  const routes = [
+    "",
+    "/data-grid",
+    "/data-grid-live",
+    "/data-grid-multiplayer",
+  ].map((route) => ({
     url: `${siteConfig.url}${route}`,
     lastModified: new Date().toISOString(),
   }));
